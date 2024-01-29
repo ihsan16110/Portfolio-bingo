b7ca6cd80d94f45a31e86afacda541764818b5aa
Mahdi Ihsan <Mahdi Ihsan>
2024-01-29 09:10:07 +0600

    mobile responsive for 768 display

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 const Header = () => {
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
+  const stickyHeaderFunc = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+  };
+
+  useEffect(() => {
+    stickyHeaderFunc();
+    return window.removeEventListener("scroll", stickyHeaderFunc);
+  }, []);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const targetAttr = e.target.getAttribute("href");
+    const location = document.querySelector(targetAttr).offsetTop;
+
+    window.scrollTo({
+      top: location - 80,
+      left: 0,
+    });
+  };
+
+  const toogleMenu = () => menuRef.current.classList.toggle("show__menu");
+
   return (
-    <header className="w-full h-[80px] leading-[80px] flex items-center">
+    <header
+      ref={headerRef}
+      className="w-full h-[80px] leading-[80px] flex items-center"
+    >
       <div className="container">
         <div className="flex items-center justify-between">
           {/*=================== logo Start ====================*/}
@@ -24,26 +61,41 @@ const Header = () => {
           {/*=================== logo End ====================*/}
 
           {/*================== Menu Start==================== */}
-          <div className="menu">
+          <div className="menu" ref={menuRef} onClick={toogleMenu}>
             <ul className="flex items-center gap-10">
               <li>
-                <a className="text-smallTextColor font-[600]" href="#about">
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#about"
+                >
                   About
                 </a>
               </li>
               <li>
-                <a className="text-smallTextColor font-[600]" href="#service">
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#service"
+                >
                   Service
                 </a>
               </li>
               <li>
-                <a className="text-smallTextColor font-[600]" href="#portfolio">
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#portfolio"
+                >
                   Portfolio
                 </a>
               </li>
               <li>
-                <a className="text-smallTextColor font-[600]" href="contact">
-                  {" "}
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="contact"
+                >
                   Contact
                 </a>
               </li>
@@ -57,7 +109,10 @@ const Header = () => {
           <button className="flex items-center gap-2 text-smallTextColor font-[600] border-solid border border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] ease-in duration-300">
             <i class="ri-send-plane-line"> Let's Talk</i>
           </button>
-          <span className="text-2xl text-smallTextColor md:hidden cursor-pointer ">
+          <span
+            onClick={toString}
+            className="text-2xl text-smallTextColor md:hidden cursor-pointer "
+          >
             <i class="ri-menu-line"></i>
           </span>
 
--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import portfolios from "../../assets/data/portfolioData";
-import imgUrl from "../../assets/data/portfolioData";
-import siteUrl from "../../assets/data/portfolioData";
 
 const Modal = ({ activeID, setShowModal }) => {
   const portfolio = portfolios.find((portfolio) => portfolio.id === activeID);
-  console.log(portfolio);
-  console.log(activeID);
+  // console.log(portfolio);
+  // console.log(activeID);
   return (
     <div className="w-full h-full fixed top-0 left-0 z-10 bg-headingColor bg-opacity-40">
       <img src={portfolio?.imgUrl} alt="" />
-      <div className="max-w-[60px] absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform-translate-x-1/2 -translate-y-1/2 p-5">
+      <div className="w-11/12 md:max-w-[600px] md:w-full absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform-translate-x-1/2 -translate-y-1/2 p-5">
         <div>
           <figure>
-            <img className="rounded0=-[8px]" src={portfolio?.imgUrl} alt="" />
+            <img className="rounded=-[8px]" src={portfolio?.imgUrl} alt="" />
           </figure>
 
           <div>
